92f0ec7728ed7a70dc029caf4634cb13733c673a
Nolan Shirley <nolanbshirley@gmail.com>
2021-01-13 11:03:34 -0500

    added unique feature to email then force trued the database

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('user', {
         email: {
             type: DataTypes.STRING, 
-            allowNull: false
+            allowNull: false, 
+            unique: true
         }, 
         password: {
             type: DataTypes.STRING, 
